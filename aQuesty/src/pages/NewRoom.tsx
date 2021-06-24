4a750b6c251cb950ed558949c8559bc5790ec437
ismael <albertismael7@gmail.com>
2021-06-23 22:33:43 -0300

    navigation

--- aQuesty/src/pages/NewRoom.tsx
+++ aQuesty/src/pages/NewRoom.tsx
@@ -1,3 +1,6 @@
+
+import { Link } from 'react-router-dom';
+ 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg';
 import '../styles/newRoom.scss';
@@ -26,7 +29,7 @@ export function NewRoom() {
                         </Button>          
                 </form>
                 <p>
-                    Quer entrar em uma sala existente? <a href=" ">Clique aqui</a>
+                    Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link>
                 </p>
             </div>
         </main>
